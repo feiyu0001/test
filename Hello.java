7cfc8899c76dc174ab1fcd7c5ee7b5b2178611c8
chedi <chedi007@163.com>
2017-08-26 17:27:56 +0800

    ti jiao ban ben!!!

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("hhhhhhhhhh");
-		System.out.println("qqqqqqqqqqqqqqqqqqqqqqq");
-		System.out.println("11111");
-		System.out.println("22222");
 	}
 }
